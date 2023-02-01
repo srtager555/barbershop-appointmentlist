b46b052a728598792d1ddfd67258215adb9977da
Carlos && Ponce <srtager555@gmail.com>
2023-01-31 22:08:08 -0600

    I tried to use the prettier CLI, sucessfull try

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
-import { Prata } from "@next/font/google"
-import { Background } from "src/common/background"
+import { Prata } from "@next/font/google";
+import { Background } from "src/common/background";
 
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-const prata = Prata({ preload: true, weight: "400", subsets: ["latin"]})
+const prata = Prata({ preload: true, weight: "400", subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-
-  return (<><Background /><main className={prata.className}><Component {...pageProps} /></main></>)
+	return (
+		<>
+			<Background />
+			<main className={prata.className}>
+				<Component {...pageProps} />
+			</main>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
