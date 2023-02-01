1f0b1170cc0890e540db70cb25cea1255e6afe0c
Carlos && Ponce <srtager555@gmail.com>
2023-02-01 00:18:33 -0600

    First version to the start page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image"
-import type { NextPage } from 'next'
-import styles from "@styles/Home.module.scss"
+import { Logo } from "src/common/logo";
+import Link from "next/link";
+import type { NextPage } from "next";
 
+import styles from "@styles/Home.module.scss";
 
-const Home: NextPage = () => {
-  const { container } = styles
-  
-  return (
-    <div>
-      <div className=""> 
-       <Image className="" src='/bigoteJossiel.png' alt='un bigote piola'  />
-      </div>
-    </div> 
-  )
-}
+const Home: NextPage = () => (
+	<div className={styles.container}>
+		<Logo />
+		<h1 className={styles.title}>Barberia Jossiel</h1>
+		<Link href="/citas" className={styles["circle-link"]}>
+			<span>Entrar</span>
+		</Link>
+	</div>
+);
 
-export default Home
+export default Home;
