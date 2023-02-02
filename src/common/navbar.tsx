2af8e432bd2f81b92edfbdd45c29285449fc3fce
Carlos && Ponce <srtager555@gmail.com>
2023-02-02 03:09:01 -0600

    I started the logic to the navbar

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,27 +1,40 @@
-import Link from "next/link";
-
+import React, { useState } from "react";
 import { Logo } from "./logo";
 import { ActiveLink } from "./ActiveLink";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+interface ComponentsList {
+  path: string;
+  component: () => JSX.Element
+}
 
 export function Navbar() {
+  const router = useRouter()
+
+  const [Nav, setNav] = useState<{ component: () => JSX.Element }>({component: AppointmentContent })
+
+  useEffect(() => {
+    const content: Array<ComponentsList> = [{ path: "citas/", component: AppointmentContent}]
+
+    const newContent = content.find((element) => element.path === router.asPath)
+
+    if (newContent) {
+      setNav({ component: newContent.component})
+    }
+  }, [router.asPath])
+
   return (
     <nav>
       <Logo />
       <div className="custom-content">
-
+        { Nav.component() }
       </div>
     </nav>
   );
 }
 
-const DateNav = () => {
-  const router = useRouter()
-
-  const ActivePathStyle = {
-    color: router.asPath  "#DDD"
-  } 
-
+const AppointmentContent = () => {
   return (
     <>
       <h2>Reservas</h2>
