4253d73eecafc4bf2be59eaa5c713a5307b9d3e3
Carlos && Ponce <srtager555@gmail.com>
2023-02-02 19:36:48 -0600

    I fixed the issue when the path changes

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,51 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { Logo } from "./logo";
 import { ActiveLink } from "./ActiveLink";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+
+import styles from "@styles/navbar.module.scss";
 
 interface ComponentsList {
-  path: string;
-  component: () => JSX.Element
+	path: string;
+	navClass: string;
+	component: () => JSX.Element;
 }
 
 export function Navbar() {
-  const router = useRouter()
+	const router = useRouter();
 
-  const [Nav, setNav] = useState<{ component: () => JSX.Element }>({component: AppointmentContent })
+	const [customClass, setCustomClass] = useState(styles.start);
+	const [Nav, setNav] = useState<{ component: () => JSX.Element }>({ component: unClasico });
 
-  useEffect(() => {
-    const content: Array<ComponentsList> = [{ path: "citas/", component: AppointmentContent}]
+	useEffect(() => {
+		const content: Array<ComponentsList> = [
+			{ path: "/citas", navClass: styles.apointments, component: AppointmentContent },
+			{ path: "/", navClass: styles.start, component: unClasico },
+		];
 
-    const newContent = content.find((element) => element.path === router.asPath)
+		const newContent = content.find((element) => element.path === router.asPath);
 
-    if (newContent) {
-      setNav({ component: newContent.component})
-    }
-  }, [router.asPath])
+		if (newContent) {
+			setNav({ component: newContent.component });
+			setCustomClass(newContent.navClass);
+		} else {
+			setNav({ component: unClasico });
+			setCustomClass("");
+		}
+	}, [router.asPath]);
 
-  return (
-    <nav>
-      <Logo />
-      <div className="custom-content">
-        { Nav.component() }
-      </div>
-    </nav>
-  );
+	return (
+		<nav className={`${styles.container} ${customClass}`}>
+			<Logo />
+			<div className={`custom-content`}>{Nav.component()}</div>
+		</nav>
+	);
 }
 
 const AppointmentContent = () => {
-  return (
-    <>
-      <h2>Reservas</h2>
-      <div className="appointment-day">
-        <ActiveLink href="citas/">Hoy</ActiveLink>
-        <span className="line"></span>
-        <ActiveLink href="citas/">Mañana</ActiveLink>
-      </div>
-    </>
-  )
-}
+	return (
+		<>
+			<h2>Reservas</h2>
+			<div className="appointment-day">
+				<ActiveLink href="citas/">Hoy</ActiveLink>
+				<span className="line"></span>
+				<ActiveLink href="citas/">Mañana</ActiveLink>
+			</div>
+		</>
+	);
+};
 
-
-
+const unClasico = () => <span></span>;
