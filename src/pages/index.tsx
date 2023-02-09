9869d4442fd03f37d04bcaa76898389f763de975
Carlos && Ponce <srtager555@gmail.com>
2023-02-09 01:29:37 -0600

    I added a redirect if the session it's null or undefined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 import type { NextPage } from "next";
 
 import styles from "@styles/Home.module.scss";
 
-const Home: NextPage = () => (
-	<div className={styles.container}>
-		<div className={styles["container-content"]}>
-			<h1 className={styles.title}>Barberia Jossiel</h1>
-			<Link href="/citas" className={styles["circle-link"]}>
-				<span>Entrar</span>
-			</Link>
+const Home: NextPage = () => {
+	const { data: session } = useSession();
+
+	return (
+		<div className={styles.container}>
+			<div className={styles["container-content"]}>
+				<h1 className={styles.title}>Barberia Jossiel</h1>
+				<Link href={session ? "/citas" : "/login"} className={styles["circle-link"]}>
+					<span>Entrar</span>
+				</Link>
+			</div>
+			<div className={styles["social-networks"]}>
+				<a href="" target="_blank">
+					instagram
+				</a>
+				<a href="" target="_blank">
+					facebook
+				</a>
+			</div>
 		</div>
-		<div className={styles["social-networks"]}>
-			<a href="" target="_blank">instagram</a>
-			<a href="" target="_blank">facebook</a>
-		</div>
-	</div>
-);
+	);
+};
 
 export default Home;
