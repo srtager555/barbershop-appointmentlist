f3bbee2adb7872abd3dc30bb138017cab7a6a679
Carlos && Ponce <srtager555@gmail.com>
2023-02-09 00:35:20 -0600

    Testing the user session logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,40 @@
 import { NextPage } from "next";
+import { FormEvent, useState } from "react";
 
 const Login: NextPage = () => {
-  return <h1>Login</h1>
-}
+	const checkData = async (e: FormEvent) => {
+		e.preventDefault();
 
-export default Login
+		const data = await fetch("http://localhost:3000/api/checkUser/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			//@ts-ignore
+			body: JSON.stringify({ phoneNumber: Number(e.currentTarget.number.value) }),
+		})
+
+    console.log(data.json())
+	};
+
+	return (
+    <>
+      <form onSubmit={(e) => checkData(e)}>
+        <p>number</p>
+        <input id="number" name="number" type="number" placeholder="+504 95873245" required />
+        {/* <p>password</p>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="tu contraseña secreta"
+          required
+        /> */}
+        <br />
+        <button>buscar data</button>
+      </form>
+    </>
+	);
+};
+
+export default Login;
