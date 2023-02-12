3b160fb82c278d3fe184e86e2644f135feb2669c
Carlos && Ponce <srtager555@gmail.com>
2023-02-12 04:17:19 -0600

    testing things

--- src/pages/api/checkUser.ts
+++ src/pages/api/checkUser.ts
@@ -6,13 +6,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { phoneNumber } = req.body;
+  const { phone } = req.body;
 
-  const exists = await Prisma.users.findUnique({
-    where: {
-      phone: phoneNumber,
-    },
-  });
+  if (phone) {
+    const exists = await Prisma.users.findUnique({
+      where: {
+        phone
+      },
+    });
+  
+    res.status(200).json({ exists, phone});
+  } else {
+    const users = await Prisma.users.findMany()
 
-  res.status(200).json(exists);
+    res.status(200).json({ hola: "hola", users, number: `xd ${phone}`})
+  }
 }
