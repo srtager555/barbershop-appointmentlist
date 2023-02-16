9b69c6fe76f2d3f5615d9221378a8a59c9a9dfa9
Carlos && Ponce <srtager555@gmail.com>
2023-02-16 01:54:23 -0600

    I created the appointment not available button

---
+++ src/components/appointments/ClosedTime.btn.tsx
@@ -0,0 +1,13 @@
+import styles from "@styles/citas.module.scss";
+
+export const AvailableTimeBTN = ({ index, time, stateStyles, callback }: appointmentsButtons) => (
+	<button
+		key={`${index} - ${time}`}
+		onClick={() => callback()}
+		className={`${styles["appointment-btn"]} ${styles.close}`}
+	>
+		<span className={styles.time}>{time}</span>
+		<span className={styles.line}></span>
+		<span className={stateStyles}>PUERTAS CERRADAS</span>
+	</button>
+);
