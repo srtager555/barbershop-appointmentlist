24b1bc7d96e00a71bd4080b94bfbe60accd6f0f7
Carlos && Ponce <srtager555@gmail.com>
2023-02-20 08:56:29 -0600

    I fixed an error when is feb and I started to add styles and more info

--- src/components/appointments/AppointmentDays.tsx
+++ src/components/appointments/AppointmentDays.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link"
 import { TODAY, months, week } from "@common/timeData";
+
+import styles from "@styles/depues.module.scss"
 
 export function AppointmentDays(setDataDays: any) {
 	function handlerCreateDate(daysToPlus: number) {
@@ -6,9 +9,29 @@ export function AppointmentDays(setDataDays: any) {
 		let month = TODAY.getUTCMonth();
 		let year = TODAY.getUTCFullYear();
 
-		if (date > 31) {
-			date = date - 31;
-			month = month + 1;
+		const isLeapYears = (year: number) => {
+			return (year % 400 === 0) ? true : 
+						(year % 100 === 0) ? false : 
+							year % 4 === 0;
+		};
+
+		if (month === 1) {
+			if (isLeapYears(year)) {
+				if (date > 29) {
+					date = date - 29;
+					month = month + 1;
+				}
+			} else {
+				if (date > 28) {
+					date = date - 28;
+					month = month + 1;
+				}
+			}
+		} else {
+			if (date > 31) {
+				date = date - 31;
+				month = month + 1;
+			}
 		}
 
 		if (month > 12) {
@@ -31,16 +54,21 @@ export function AppointmentDays(setDataDays: any) {
 	}
 
 	return (
-		<div className={style.container}>
-			{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((el) => {
-				const TIME = new Date(handlerCreateDate(el));
+		<div className={styles.container}>
+			<button className={styles.toTop} onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>⇑</button>
+			<Link href="/citas" className={styles.return}><span className={styles.arrow}>↼</span> <span className={styles.text}>Regresar al presente</span></Link>
+			<p className={styles["example-text"]}>Selecciona uno de los siguientes días</p>
+			<div className={styles.container__button}>
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((el) => {
+					const TIME = new Date(handlerCreateDate(el));
 
-				return (
-					<button key={`btn-${el}`} onClick={() => handlerSetDate(el)}>
-						{week[TIME.getUTCDay()]} - {TIME.getUTCDate()}
-					</button>
-				);
-			})}
+					return (
+						<button className={styles.button} key={`btn-${el}`} onClick={() => handlerSetDate(el)}>
+							{week[TIME.getUTCDay()]} - {TIME.getUTCDate()}
+						</button>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
