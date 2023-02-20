074e2f46a70e5ac1617f971548f8e67575c10c06
Carlos && Ponce <srtager555@gmail.com>
2023-02-20 09:08:39 -0600

    I created the logic to show the button to top when the scroll is upper to 400

--- src/components/appointments/AppointmentDays.tsx
+++ src/components/appointments/AppointmentDays.tsx
@@ -1,18 +1,24 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { TODAY, months, week } from "@common/timeData";
 
-import styles from "@styles/depues.module.scss"
+import styles from "@styles/depues.module.scss";
 
 export function AppointmentDays(setDataDays: any) {
+	const [isOverScroll, setIsOverScroll] = useState(false);
+
+	function handlerScroll() {
+		if (window.scrollY > 400) setIsOverScroll(true);
+		else setIsOverScroll(false);
+	}
+
 	function handlerCreateDate(daysToPlus: number) {
 		let date = TODAY.getUTCDate() + daysToPlus;
 		let month = TODAY.getUTCMonth();
 		let year = TODAY.getUTCFullYear();
 
 		const isLeapYears = (year: number) => {
-			return (year % 400 === 0) ? true : 
-						(year % 100 === 0) ? false : 
-							year % 4 === 0;
+			return year % 400 === 0 ? true : year % 100 === 0 ? false : year % 4 === 0;
 		};
 
 		if (month === 1) {
@@ -53,17 +59,37 @@ export function AppointmentDays(setDataDays: any) {
 		);
 	}
 
+	useEffect(() => {
+		window.addEventListener("scroll", handlerScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handlerScroll);
+		};
+	}, []);
+
 	return (
 		<div className={styles.container}>
-			<button className={styles.toTop} onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>⇑</button>
-			<Link href="/citas" className={styles.return}><span className={styles.arrow}>↼</span> <span className={styles.text}>Regresar al presente</span></Link>
+			<button
+				className={`${styles.toTop} ${isOverScroll ? styles.show : ""}`}
+				onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+			>
+				⇑
+			</button>
+			<Link href="/citas" className={styles.return}>
+				<span className={styles.arrow}>↼</span>{" "}
+				<span className={styles.text}>Regresar al presente</span>
+			</Link>
 			<p className={styles["example-text"]}>Selecciona uno de los siguientes días</p>
 			<div className={styles.container__button}>
 				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((el) => {
 					const TIME = new Date(handlerCreateDate(el));
 
 					return (
-						<button className={styles.button} key={`btn-${el}`} onClick={() => handlerSetDate(el)}>
+						<button
+							className={styles.button}
+							key={`btn-${el}`}
+							onClick={() => handlerSetDate(el)}
+						>
 							{week[TIME.getUTCDay()]} - {TIME.getUTCDate()}
 						</button>
 					);
