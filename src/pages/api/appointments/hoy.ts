c2a5c409867a5fa875814482a6be3488fc75f814
Carlos && Ponce <srtager555@gmail.com>
2023-02-19 21:55:27 -0600

    new syntax to the new data

--- src/pages/api/appointments/hoy.ts
+++ src/pages/api/appointments/hoy.ts
@@ -1,11 +1,44 @@
-import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { TimeList } from "@common/timeList";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!req.body) {
-    const TODAY_APOINTMENTS = await Prisma?.apoint_today.findMany()
-  
-    res.status(200).json(TODAY_APOINTMENTS)
-  } else {
-    const {} = req.body
-  }
-} 
+	if (!req.body) {
+		const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+		const TODAY = new Date();
+		const UTC_TODAY = `${week[TODAY.getUTCDay()]} ${TODAY.getUTCDate} ${TODAY.getUTCMonth}M ${
+			TODAY.getUTCFullYear
+		}}`;
+
+		let OPENING_CLOSING =
+			(await Prisma?.custom_oc.findUnique({
+				where: {
+					date: UTC_TODAY,
+				},
+			})) ||
+			(await Prisma?.custom_oc.findUnique({
+				where: {
+					date: "default",
+				},
+			}));
+
+		const TODAY_APOINTMENTS = await Prisma?.appointments.findMany({
+			where: {
+				date: UTC_TODAY,
+			},
+		});
+
+		const TODAY_CLOSED_TIME = await Prisma?.custom_closed_time.findMany({
+			where: {
+				date: UTC_TODAY,
+			},
+		});
+
+		const PROCESS_TODAY = TimeList(OPENING_CLOSING, TODAY_APOINTMENTS, TODAY_CLOSED_TIME);
+
+		res.status(200).json(PROCESS_TODAY);
+	} else {
+		const {} = req.body;
+	}
+}
