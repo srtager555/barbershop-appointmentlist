34ec7c689b51d787f5ea2d2cc011f7fc9e8025e8
Carlos && Ponce <srtager555@gmail.com>
2023-02-20 00:57:52 -0600

    I changed the file name

---
+++ src/pages/citas/despues.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+import { useState } from "react"
+
+const Despues: NextPage = () => {
+  const [showDays, setShowDays] = useState(true)
+
+  if (showDays) return 
+}
+
+export default Despues
