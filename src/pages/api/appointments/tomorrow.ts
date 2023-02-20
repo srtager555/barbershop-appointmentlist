f978d57cfde73fc64d75f531359716ed3533aea2
Carlos && Ponce <srtager555@gmail.com>
2023-02-20 02:51:18 -0600

    I started the api to tomorrow

---
+++ src/pages/api/appointments/tomorrow.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { TimeList } from "@common/timeList";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { date } = req.body
+
+
+
+
+}
