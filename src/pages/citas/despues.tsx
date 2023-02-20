2e84b8f9f59532a6bbe92eed09661f163e5bfacc
Carlos && Ponce <srtager555@gmail.com>
2023-02-20 02:51:39 -0600

    I created the first part to tomorrow

--- src/pages/citas/despues.tsx
+++ src/pages/citas/despues.tsx
@@ -1,11 +1,15 @@
+import { AppointmentDays } from "@components/appointments/AppointmentDays"
+import Layout from "@components/appointments/Layout"
 import { NextPage } from "next"
 
 import { useState } from "react"
 
 const Despues: NextPage = () => {
-  const [showDays, setShowDays] = useState(true)
+  const [dataDays, setDataDays] = useState<appointmentData[]>([])
 
-  if (showDays) return 
+  if (dataDays) return <AppointmentDays setDataDays={setDataDays} />
+
+  return <Layout data={[]} />
 }
 
 export default Despues
