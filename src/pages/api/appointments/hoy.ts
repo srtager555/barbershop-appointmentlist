44b8a16b2937909020930bb3915c43fde4835c08
Carlos && Ponce <srtager555@gmail.com>
2023-02-21 01:41:48 -0600

    I changed the queries

--- src/pages/api/appointments/hoy.ts
+++ src/pages/api/appointments/hoy.ts
@@ -3,33 +3,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { TimeList } from "@common/timeList";
 import { week, UTC_TODAY, TODAY } from "@common/timeData"
 import { AppointmentReducer } from "@common/appointmentReducer";
+import { OPENING_CLOSING, DATE_APOINTMENTS, DATE_CLOSED_TIME } from "@ddbb/prisma.queries";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-		let OPENING_CLOSING =
-			(await Prisma?.layout_day.findUnique({
-				where: {
-					day: week[TODAY.getUTCDay()],
-				},
-			})) ||
-			(await Prisma?.layout_day.findUnique({
-				where: {
-					day: "default",
-				},
-			}));
+		const OP_LAYOUT = await OPENING_CLOSING(TODAY.getUTCDay())
 
-		const TODAY_APOINTMENTS = await Prisma?.appointments.findMany({
-			where: {
-				date: UTC_TODAY,
-			},
-		});
+		const TODAY_APOINTMENTS = await DATE_APOINTMENTS(UTC_TODAY)
 
-		const TODAY_CLOSED_TIME = await Prisma?.custom_closed_time.findMany({
-			where: {
-				date: UTC_TODAY,
-			},
-		});
+		const TODAY_CLOSED_TIME = await DATE_CLOSED_TIME(UTC_TODAY)
 
-		const TODAY_LIST = TimeList(OPENING_CLOSING, TODAY_APOINTMENTS, TODAY_CLOSED_TIME);
+		const TODAY_LIST = TimeList(OP_LAYOUT, TODAY_APOINTMENTS, TODAY_CLOSED_TIME);
 		const PROCESS_TODAY = AppointmentReducer(TODAY_LIST)
 
 		res.status(200).json(PROCESS_TODAY);
