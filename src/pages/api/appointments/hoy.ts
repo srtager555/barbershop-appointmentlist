2fc7c749909908eba1bc2ea001408bf171845a8e
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 17:04:19 -0600

    I fixed a typing error

--- src/pages/api/appointments/hoy.ts
+++ src/pages/api/appointments/hoy.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { TimeList } from "@common/timeList";
-import { week, UTC_TODAY, TODAY } from "@common/timeData"
+import { week, TODAY_CUSTOM, TODAY } from "@common/timeData"
 import { AppointmentReducer } from "@common/appointmentReducer";
 import { OPENING_CLOSING, DATE_APOINTMENTS, DATE_CLOSED_TIME } from "@ddbb/prisma.queries";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 		const OP_LAYOUT = await OPENING_CLOSING(TODAY.getDay())
 
-		const TODAY_APOINTMENTS = await DATE_APOINTMENTS(UTC_TODAY)
+		const TODAY_APOINTMENTS = await DATE_APOINTMENTS(TODAY_CUSTOM)
 
-		const TODAY_CLOSED_TIME = await DATE_CLOSED_TIME(UTC_TODAY)
+		const TODAY_CLOSED_TIME = await DATE_CLOSED_TIME(TODAY_CUSTOM)
 
 		const TODAY_LIST = TimeList(OP_LAYOUT, TODAY_APOINTMENTS, TODAY_CLOSED_TIME);
 		const PROCESS_TODAY = AppointmentReducer(TODAY_LIST)
