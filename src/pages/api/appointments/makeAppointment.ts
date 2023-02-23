76b67fb65540098f259cc3f1a6f50893a0739d95
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 03:25:30 -0600

    i fixed a typing error

--- src/pages/api/appointments/makeAppointment.ts
+++ src/pages/api/appointments/makeAppointment.ts
@@ -1,9 +1,7 @@
 import { NextApiResponse, NextApiRequest } from "next";
 
 export default async function MakeAppointment(req: NextApiRequest, res: NextApiResponse) {
-  const { time, user_id, date }: { time: string; user_id: number; date: string } = req.body
-
-  console.log(time, user_id, date)
+  const { time, user_id, date }: { time: string; user_id: number; date: string } = JSON.parse(req.body)
 
   const data = await Prisma?.appointments.create({
     data: {
