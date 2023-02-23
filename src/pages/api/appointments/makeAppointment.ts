b9e08f243acd76c262aa78dbeb833d1de575bd41
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 03:09:42 -0600

    I started to create the logic to the make appointments

--- src/pages/api/appointments/makeAppointment.ts
+++ src/pages/api/appointments/makeAppointment.ts
@@ -1,5 +1,17 @@
 import { NextApiResponse, NextApiRequest } from "next";
 
 export default async function MakeAppointment(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json("ok")
+  const { time, user_id, date }: { time: string; user_id: number; date: string } = req.body
+
+  console.log(time, user_id, date)
+
+  const data = await Prisma?.appointments.create({
+    data: {
+      time,
+      user_id,
+      date
+    }
+  })
+
+  res.status(200).json(data)
 }
