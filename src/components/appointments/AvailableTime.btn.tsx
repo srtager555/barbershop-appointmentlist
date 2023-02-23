3f88d2998e056be2283c930cb2727695d733c0aa
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 03:09:03 -0600

    Im testing the fetch to the api

--- src/components/appointments/AvailableTime.btn.tsx
+++ src/components/appointments/AvailableTime.btn.tsx
@@ -3,16 +3,21 @@ import { useSession } from "next-auth/react";
 
 import styles from "@styles/citas.module.scss";
 
-export const AvailableTimeBTN = ({ time, stateStyles }: appointmentsButtons) => {
+export const AvailableTimeBTN = ({ time, stateStyles, date }: appointmentsButtons) => {
 	const router = useRouter()
 	const { data: session, status } = useSession()
 
 	const handlerUseAppointment = async () => {
-		if (status === "unauthenticated") return router.push('/login')
+		if (status === "unauthenticated" || !session) return router.push('/login')
 
-		console.log(session)
-
-		await fetch("/api/appointments/makeAppointment").then((data) => console.log(data.json()))
+		await fetch("/api/appointments/makeAppointment", {
+			method: "POST",
+			body: JSON.stringify({
+				time,
+				user_id: session.user.id,
+				date
+			})
+		}).then((data) => console.log(data.json()))
 	}
 
 	return (
