2d9eb64266becf0ba44db364a31bd75c50cb7676
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 02:12:30 -0600

    I started to create the logic to make a appointment

--- src/components/appointments/AvailableTime.btn.tsx
+++ src/components/appointments/AvailableTime.btn.tsx
@@ -1,12 +1,25 @@
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
 import styles from "@styles/citas.module.scss";
 
-export const AvailableTimeBTN = ({  time, stateStyles, callback }: appointmentsButtons) => (
-	<button
-		onClick={() => callback && callback()}
-		className={styles["appointment-btn"]}
-	>
-		<span className={styles.time}>{time}</span>
-		<span className={styles.line}></span>
-		<span className={stateStyles}>reservar puesto</span>
-	</button>
-);
+export const AvailableTimeBTN = ({ time, stateStyles }: appointmentsButtons) => {
+	const router = useRouter()
+	const { data: session, status } = useSession()
+
+	const handlerUseAppointment = async () => {
+		if (status === "unauthenticated") return router.push('/login')
+
+		console.log(session)
+
+		await fetch("/api/appointments/makeAppointment").then((data) => console.log(data.json()))
+	}
+
+	return (
+		<button onClick={handlerUseAppointment} className={styles["appointment-btn"]}>
+			<span className={styles.time}>{time}</span>
+			<span className={styles.line}></span>
+			<span className={stateStyles}>reservar puesto</span>
+		</button>
+	);
+};
