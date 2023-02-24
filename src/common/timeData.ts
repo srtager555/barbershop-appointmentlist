8636a940bf8d98c796f4b2ded4b2d74cb3711f84
Carlos && Ponce <srtager555@gmail.com>
2023-02-24 00:50:46 -0600

    I added a new feature, the current time!

--- src/common/timeData.ts
+++ src/common/timeData.ts
@@ -22,3 +22,18 @@ export const TODAY = new Date();
 export const TODAY_CUSTOM = `${months[TODAY.getMonth()]} ${TODAY.getDate()} ${TODAY.getFullYear()}`;
 
 export const UTC_TODAY = `${months[TODAY.getUTCMonth()]} ${TODAY.getUTCDate()} ${TODAY.getUTCFullYear()}}`;
+
+
+export const CURRENT_HOUR = () => {
+	const time = new Date()
+	const get_minutes = time.getMinutes()
+
+	let minute;
+
+	if (get_minutes < 20) minute = "00"
+	else if (get_minutes < 40) minute = "20"
+	else minute = "40"
+
+	// example: 00"20 || 17"40
+	return `${time.getHours() < 10 ? `0${time.getHours()}` : time.getHours()}"${minute}`
+}
