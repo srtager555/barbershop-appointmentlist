a1d1f6dbeeffc0e8b9459d34eaee4951ca503793
Carlos && Ponce <srtager555@gmail.com>
2023-02-24 00:52:01 -0600

    I did ident the code

--- src/pages/api/appointments/makeAppointment.ts
+++ src/pages/api/appointments/makeAppointment.ts
@@ -2,56 +2,57 @@ import { CURRENT_HOUR, TODAY } from "@common/timeData";
 import { TIME_LIST } from "@common/timeList";
 import { NextApiResponse, NextApiRequest } from "next";
 
-
 export default async function MakeAppointment(req: NextApiRequest, res: NextApiResponse) {
-  const { time, user_id, date, createNewAppoint }: AppointAPIBody = JSON.parse(req.body)
-  let data;
+	const { time, user_id, date, createNewAppoint }: AppointAPIBody = JSON.parse(req.body);
+	let data;
 
-  const handlerDeleteAppointment = async () => {
-    await Prisma?.appointments.delete({
-      where: {
-        user_id
-      }
-    })
-  } 
+	const handlerDeleteAppointment = async () => {
+		await Prisma?.appointments.delete({
+			where: {
+				user_id,
+			},
+		});
+	};
 
-  if (createNewAppoint) await handlerDeleteAppointment()
+	if (createNewAppoint) await handlerDeleteAppointment();
 
-  const isThereAnAppointment = await Prisma?.appointments.findUnique({
-    where: {
-      user_id,
-    }
-  }).then(async (element) => {
-    if (!element) return false
+	const isThereAnAppointment = await Prisma?.appointments
+		.findUnique({
+			where: {
+				user_id,
+			},
+		})
+		.then(async (element) => {
+			if (!element) return false;
 
-    const appointDate = new Date(element.date)
+			const appointDate = new Date(element.date);
 
-    const CONDITIONS = [
-      appointDate.getMonth() < TODAY.getMonth(),
-      appointDate.getDate() < TODAY.getDate(),
-      TIME_LIST.indexOf(element.time) < TIME_LIST.indexOf(CURRENT_HOUR())
-    ]
+			const CONDITIONS = [
+				appointDate.getMonth() < TODAY.getMonth(),
+				appointDate.getDate() < TODAY.getDate(),
+				TIME_LIST.indexOf(element.time) < TIME_LIST.indexOf(CURRENT_HOUR()),
+			];
 
-    const RESULT = CONDITIONS.some(el => el === true)
+			const RESULT = CONDITIONS.some((el) => el === true);
 
-    if (RESULT) {
-      await handlerDeleteAppointment()
-      return false
-    }
+			if (RESULT) {
+				await handlerDeleteAppointment();
+				return false;
+			}
 
-    return true
-  })
+			return true;
+		});
 
-  if (isThereAnAppointment) data = { hasAppointment: true }
-  else {
-    data = await Prisma?.appointments.create({
-      data: {
-        time,
-        user_id,
-        date
-      }
-    })
-  }
-  
-  res.status(200).json(data)
+	if (isThereAnAppointment) data = { hasAppointment: true };
+	else {
+		data = await Prisma?.appointments.create({
+			data: {
+				time,
+				user_id,
+				date,
+			},
+		});
+	}
+
+	res.status(200).json(data);
 }
