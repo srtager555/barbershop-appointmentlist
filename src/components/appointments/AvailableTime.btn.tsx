75a0da19e1dd34ad917bd4c637ade838ebc86e86
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 22:33:40 -0600

    I created the logic to changed the appointment

--- src/components/appointments/AvailableTime.btn.tsx
+++ src/components/appointments/AvailableTime.btn.tsx
@@ -1,23 +1,58 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
+import Swal from "sweetalert2";
 import styles from "@styles/citas.module.scss";
 
-export const AvailableTimeBTN = ({ time, stateStyles, date }: appointmentsButtons) => {
+const fetcher = async (body: AppointAPIBody) => await fetch("/api/appointments/makeAppointment", {
+		method: "POST",
+		body: JSON.stringify(body)
+	})
+
+export const AvailableTimeBTN = ({ time, stateStyles, date, callback }: appointmentsButtons) => {
 	const router = useRouter()
 	const { data: session, status } = useSession()
 
 	const handlerUseAppointment = async () => {
 		if (status === "unauthenticated" || !session) return router.push('/login')
 
-		await fetch("/api/appointments/makeAppointment", {
-			method: "POST",
-			body: JSON.stringify({
-				time,
-				user_id: session.user.id,
-				date
+		const RESPONSE = await fetcher({
+			time,
+			date,
+			user_id: session.user.id
+		 })
+
+		const { hasAppointment }: AppointAPIResponse = await RESPONSE.json()
+
+		if (hasAppointment) {
+			Swal.fire({
+				icon: "info",
+				title: '¡Ya tienes una reserva!',
+				text: 'Solo puedes tener una reserva la vez',
+				confirmButtonText: 'Cambiar reserva',
+				cancelButtonText: "Cancelar",
+				showCancelButton: true,
+			}).then(async (result) => {
+				if (result.isConfirmed) {
+					try {
+						await fetcher({ 
+							time, 
+							date, 
+							user_id: session.user.id, 
+							createNewAppoint: true 
+						})
+
+						Swal.fire('Hecho!', 'Tu reserva a sido cambiada', 'success')
+						if (callback) callback()
+
+					} catch (error) {
+							Swal.fire('¡Algo fallo!', 'Hubo une error al hacer la reserva', "error")
+							console.log(error)	
+					}
+				} 
 			})
-		}).then((data) => console.log(data.json()))
+		}
+
 	}
 
 	return (
