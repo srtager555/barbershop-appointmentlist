ee25f16a441629c42c5449dafc12214b1b6518e2
Carlos && Ponce <srtager555@gmail.com>
2023-02-24 00:50:23 -0600

    I created the logic to create appointments

--- src/pages/api/appointments/makeAppointment.ts
+++ src/pages/api/appointments/makeAppointment.ts
@@ -1,3 +1,5 @@
+import { CURRENT_HOUR, TODAY } from "@common/timeData";
+import { TIME_LIST } from "@common/timeList";
 import { NextApiResponse, NextApiRequest } from "next";
 
 
@@ -5,18 +7,39 @@ export default async function MakeAppointment(req: NextApiRequest, res: NextApiR
   const { time, user_id, date, createNewAppoint }: AppointAPIBody = JSON.parse(req.body)
   let data;
 
-  if (createNewAppoint) {
+  const handlerDeleteAppointment = async () => {
     await Prisma?.appointments.delete({
       where: {
         user_id
       }
     })
-  }
+  } 
+
+  if (createNewAppoint) await handlerDeleteAppointment()
 
   const isThereAnAppointment = await Prisma?.appointments.findUnique({
     where: {
       user_id,
     }
+  }).then(async (element) => {
+    if (!element) return false
+
+    const appointDate = new Date(element.date)
+
+    const CONDITIONS = [
+      appointDate.getMonth() < TODAY.getMonth(),
+      appointDate.getDate() < TODAY.getDate(),
+      TIME_LIST.indexOf(element.time) < TIME_LIST.indexOf(CURRENT_HOUR())
+    ]
+
+    const RESULT = CONDITIONS.some(el => el === true)
+
+    if (RESULT) {
+      await handlerDeleteAppointment()
+      return false
+    }
+
+    return true
   })
 
   if (isThereAnAppointment) data = { hasAppointment: true }
