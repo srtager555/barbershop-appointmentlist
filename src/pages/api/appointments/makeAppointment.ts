23c1a92136564763890b19a3eec09d219f0fa17d
Carlos && Ponce <srtager555@gmail.com>
2023-02-23 21:33:17 -0600

    Logic to the create and replace an appointment added

--- src/pages/api/appointments/makeAppointment.ts
+++ src/pages/api/appointments/makeAppointment.ts
@@ -1,15 +1,32 @@
 import { NextApiResponse, NextApiRequest } from "next";
 
-export default async function MakeAppointment(req: NextApiRequest, res: NextApiResponse) {
-  const { time, user_id, date }: { time: string; user_id: number; date: string } = JSON.parse(req.body)
 
-  const data = await Prisma?.appointments.create({
-    data: {
-      time,
+export default async function MakeAppointment(req: NextApiRequest, res: NextApiResponse) {
+  const { time, user_id, date, createNewAppoint }: AppointAPIBody = JSON.parse(req.body)
+
+  const isThereAnAppointment = await Prisma?.appointments.findUnique({
+    where: {
       user_id,
-      date
     }
   })
 
-  res.status(200).json(data)
+  if (!isThereAnAppointment && createNewAppoint) {
+    await Prisma?.appointments.delete({
+      where: {
+        user_id
+      }
+    })
+
+    const data = await Prisma?.appointments.create({
+      data: {
+        time,
+        user_id,
+        date
+      }
+    })
+
+    res.status(200).json(data)
+  }
+  
+  res.status(200).json({ hasAppointment: true, time, user_id, date })
 }
