1698ece6b6bf096250041cd8f5baaaeec8625721
Carlos && Ponce <srtager555@gmail.com>
2023-02-25 00:44:36 -0600

    I changed the function OPENING_CLOSING to LAYOUT_DAY

--- src/pages/api/appointments/tomorrow.ts
+++ src/pages/api/appointments/tomorrow.ts
@@ -1,18 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { DATE_APOINTMENTS, DATE_CLOSED_TIME, OPENING_CLOSING } from "@ddbb/prisma.queries";
+import { DATE_APOINTMENTS, DATE_CLOSED_TIME, LAYOUT_DAY } from "@ddbb/prisma.queries";
 import { TimeList } from "@common/timeList";
 import { AppointmentReducer } from "@common/appointmentReducer";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const date: string = JSON.parse(req.body).date;
 
-  if (!date) res.status(200).json("¿Que haces aquí?")
+	if (!date) res.status(200).json("¿Que haces aquí?");
 
-  const DAY_NUMBER: number = new Date(date).getDay()
+	const OP_LAYOUT = await LAYOUT_DAY(date);
 
-	const OP_LAYOUT = await OPENING_CLOSING(DAY_NUMBER);
-  
 	const TODAY_APOINTMENTS = await DATE_APOINTMENTS(date);
 
 	const TODAY_CLOSED_TIME = await DATE_CLOSED_TIME(date);
