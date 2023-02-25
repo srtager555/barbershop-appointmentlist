3ad4f38353a514a540a90e79cc5b07e138c5bac9
Carlos && Ponce <srtager555@gmail.com>
2023-02-24 21:37:04 -0600

    I created the logic to set avalaible btns

--- src/components/appointments/AvailableTime.btn.tsx
+++ src/components/appointments/AvailableTime.btn.tsx
@@ -1,62 +1,80 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { VALID_TIME_TO_APPOINT } from "@common/timeData";
 
 import Swal from "sweetalert2";
 import styles from "@styles/citas.module.scss";
 
-const fetcher = async (body: AppointAPIBody) => await fetch("/api/appointments/makeAppointment", {
+const fetcher = async (body: AppointAPIBody) =>
+	await fetch("/api/appointments/makeAppointment", {
 		method: "POST",
-		body: JSON.stringify(body)
-	})
+		body: JSON.stringify(body),
+	});
 
 export const AvailableTimeBTN = ({ time, stateStyles, date, callback }: appointmentsButtons) => {
-	const router = useRouter()
-	const { data: session, status } = useSession()
+	const [availableTime, setAvailableTime] = useState(false);
+	const router = useRouter();
+	const { data: session, status } = useSession();
 
 	const handlerUseAppointment = async () => {
-		if (status === "unauthenticated" || !session) return router.push('/login')
+		if (status === "unauthenticated" || !session) return router.push("/login");
 
 		const RESPONSE = await fetcher({
 			time,
 			date,
-			user_id: session.user.id
-		 })
+			user_id: session.user.id,
+		});
 
-		const { hasAppointment }: AppointAPIResponse = await RESPONSE.json()
+		const { hasAppointment }: AppointAPIResponse = await RESPONSE.json();
 
 		if (hasAppointment) {
 			Swal.fire({
 				icon: "info",
-				title: '¡Ya tienes una reserva!',
-				text: 'Solo puedes tener una reserva la vez',
-				confirmButtonText: 'Cambiar reserva',
+				title: "¡Ya tienes una reserva!",
+				text: "Solo puedes tener una reserva a la vez",
+				confirmButtonText: "Cambiar reserva",
 				cancelButtonText: "Cancelar",
 				showCancelButton: true,
 			}).then(async (result) => {
 				if (result.isConfirmed) {
 					try {
-						await fetcher({ 
-							time, 
-							date, 
-							user_id: session.user.id, 
-							createNewAppoint: true 
-						})
+						await fetcher({
+							time,
+							date,
+							user_id: session.user.id,
+							createNewAppoint: true,
+						});
 
-						Swal.fire('Hecho!', 'Tu reserva a sido cambiada', 'success')
-						if (callback) callback()
-
+						Swal.fire("Hecho!", "Tu reserva a sido cambiada", "success");
+						if (callback) callback();
 					} catch (error) {
-							Swal.fire('¡Algo fallo!', 'Hubo une error al hacer la reserva', "error")
-							console.log(error)	
+						Swal.fire("¡Algo fallo!", "Hubo une error al hacer la reserva", "error");
+						console.log(error);
 					}
-				} 
-			})
+				}
+			});
 		}
+	};
 
-	}
+	useEffect(() => {
+		setAvailableTime(VALID_TIME_TO_APPOINT(time, date));
+
+		setInterval(() => {
+			const VALID = VALID_TIME_TO_APPOINT(time, date);
+
+			if (VALID != availableTime) setAvailableTime(VALID);
+		}, 10000);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
-		<button onClick={handlerUseAppointment} className={styles["appointment-btn"]}>
+		<button
+			onClick={handlerUseAppointment}
+			className={`${styles["appointment-btn"]}`}
+			disabled={availableTime}
+		>
 			<span className={styles.time}>{time}</span>
 			<span className={styles.line}></span>
 			<span className={stateStyles}>reservar puesto</span>
