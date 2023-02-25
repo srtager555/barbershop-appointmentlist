5d438b8b8653e5760be96f540695b2fc70596e00
Carlos && Ponce <srtager555@gmail.com>
2023-02-24 19:56:59 -0600

    API logic to drop an appointment created xd

--- src/pages/api/appointments/dropAppointment.ts
+++ src/pages/api/appointments/dropAppointment.ts
@@ -1,5 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function DropAppointment(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json("Mañana tengo que terminar esto xd")
+  const { user_id } = JSON.parse(req.body)
+
+  const DROP = await Prisma?.appointments.delete({
+    where:{
+      user_id
+    }
+  })
+
+  res.status(200).json(DROP)
 }
