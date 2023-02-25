6f491441b9e39a922049dc9a66510404f55dd2b6
Carlos && Ponce <srtager555@gmail.com>
2023-02-25 00:41:28 -0600

    I changed the var week and I added a new var

--- src/common/timeData.ts
+++ src/common/timeData.ts
@@ -1,4 +1,4 @@
-export const week = ["Dom", "Lun", "Mar", "Mier", "Jue", "Vier", "Sab"];
+export const week = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
 
 export const months = [
 	"Enero",
@@ -34,6 +34,10 @@ export const TODAY = new Date();
 
 // example: December 15 2023
 export const TODAY_CUSTOM = `${months[TODAY.getMonth()]} ${TODAY.getDate()} ${TODAY.getFullYear()}`;
+
+export const TODAY_CUSTOM_EN = `${
+	monthsEnglish[TODAY.getMonth()]
+} ${TODAY.getDate()} ${TODAY.getFullYear()}`;
 
 export const UTC_TODAY = `${
 	months[TODAY.getUTCMonth()]
