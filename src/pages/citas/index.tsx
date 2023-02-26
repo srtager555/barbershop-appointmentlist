022b8e121232fd1550215ed89fa9f039c398326e
Carlos && Ponce <srtager555@gmail.com>
2023-02-25 23:44:10 -0600

    I imported the loader

--- src/pages/citas/index.tsx
+++ src/pages/citas/index.tsx
@@ -1,19 +1,18 @@
-import { useState, useEffect } from "react"
 import { NextPage } from "next";
 
 import Layout from "@components/appointments/Layout";
 import useSWR from "swr";
-import { AppointmentReducer } from "@common/appointmentReducer";
+import { Loader } from "@common/Loader";
 
 const fetcher = (url: string) => fetch(url).then((data) => data.json());
 
 const Hoy: NextPage = () => {
 	const { data, error, isLoading } = useSWR(`/api/appointments/hoy`, fetcher);
 
-	if (error) return <span>Hubo un error</span>
-	if (isLoading) return <span>Loading...</span>
+	if (error) return <span>Hubo un error</span>;
+	if (isLoading) return <Loader />;
 
-  return <Layout data={data} />
-}
+	return <Layout data={data} />;
+};
 
-export default Hoy
+export default Hoy;
--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,10 +1,17 @@
+import { Loader } from "@common/Loader";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Profile: NextPage = () => {
-  const session = useSession()
+	const { data: session, status } = useSession();
+	const router = useRouter();
 
-  return <></>
-}
+	if (status === "loading") return <Loader />;
+
+	if (status === "unauthenticated") router.push("/login");
+
+	return <>?</>;
+};
 
 export default Profile;
