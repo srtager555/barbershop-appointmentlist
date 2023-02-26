8c9343f775333aa0440e650d69f0da3e940009f9
Carlos && Ponce <srtager555@gmail.com>
2023-02-25 18:25:23 -0600

    I created create user and singin layout with styles

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,34 +4,60 @@ import Link from "next/link";
 import styles from "@styles/Form.module.scss";
 
 const Form = ({ type, callback }: { type: "login" | "regis"; callback: Function }) => (
-	<>
-		<h2 className={styles.title}>{type === "login" ? "Inicia Sesion" : "Crea una cuenta"}</h2>
+	<div className={`${styles.container}`}>
+		<p className={styles.title}>
+			{type === "login" ? (
+				<>
+					{"¡Inicia sesión! o "}
+					<Link href="/registrar" className={styles.cta}>
+						¿no tienes una cuenta?
+					</Link>
+				</>
+			) : (
+				<>
+					{"¡Crea una cuenta! o"}
+					<Link href="/login" className={styles.cta}>
+						¿ya tienes una?
+					</Link>
+				</>
+			)}
+		</p>
 		<form className={styles["form"]} onSubmit={(e: FormEvent) => callback(e)}>
 			{type === "login" ? (
 				""
 			) : (
 				<>
-					<p>Ingreda tu nombre</p>
-					<input id="name" name="name" type="text" required />
+					<div className={styles.inputContainer}>
+						<input
+							className={styles.input}
+							id="name"
+							name="name"
+							type="text"
+							required
+						/>
+						<span className={styles.name}>Primer Nombre y Apellido</span>
+					</div>
 				</>
 			)}
-			<p>number</p>
-			<input id="phone" name="phone" type="text" placeholder="+504 95873245" required />
-			<p>password</p>
-			<input
-				id="password"
-				name="password"
-				type="password"
-				placeholder="tu contraseña secreta"
-				required
-			/>
-			<br />
-			<button>buscar data</button>
+			<div className={styles.inputContainer}>
+				<input className={styles.input} id="phone" name="phone" type="text" required />
+				<span className={styles.name}>Tu número</span>
+			</div>
+			<div className={styles.inputContainer}>
+				<input
+					className={styles.input}
+					id="password"
+					name="password"
+					type="password"
+					required
+				/>
+				<span className={styles.name}>Tu contraseña</span>
+			</div>
+			<button className={styles["btn-action"]}>
+				{type === "login" ? "Buscar cuenta" : "Crear cuenta"}
+			</button>
 		</form>
-		<Link href={type === "login" ? "/registrar" : "/login"}>
-			{type === "login" ? "Crear cuenta" : "¿Ya tienes una cuenta?"}
-		</Link>
-	</>
+	</div>
 );
 
 export default Form;
