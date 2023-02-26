1591f5267aacf6beb26bac6455e8ea3f63b67dbc
Carlos && Ponce <srtager555@gmail.com>
2023-02-26 01:48:56 -0600

    I created the fetching to the user's appointment

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 import { Loader } from "@common/Loader";
 import Image from "next/image";
@@ -10,6 +11,19 @@ import styles from "@styles/Perfil.module.scss";
 const Profile: NextPage = () => {
 	const { data: session, status } = useSession();
 	const router = useRouter();
+	const [appointment, setAppoinment] = useState();
+
+	useEffect(() => {
+		const hasAppointment = async () =>
+			await fetch("/api/appointments/userAppointment", {
+				method: "POST",
+				body: JSON.stringify({
+					user_id: session?.user.id,
+				}),
+			}).then(async (data) => setAppoinment(await data.json()));
+
+		hasAppointment();
+	}, [session]);
 
 	if (status === "loading") return <Loader />;
 
