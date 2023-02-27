97b65062a6b83fbddb8654a4eccc8a2a48952ce7
Carlos && Ponce <srtager555@gmail.com>
2023-02-26 20:39:43 -0600

    util to warn the user about close the session

---
+++ src/utils/signOut.ts
@@ -0,0 +1,16 @@
+import { signOut } from "next-auth/react";
+
+import Swal from "sweetalert2";
+
+export const handlerSignOut = () => {
+	Swal.fire({
+		title: "¡Ojo!",
+		text: "Cerraras la sesión actual",
+		icon: "info",
+		confirmButtonText: "Cerrar",
+		showCancelButton: true,
+		cancelButtonText: "Mejor no...",
+	}).then((data) => {
+		if (data.isConfirmed) signOut();
+	});
+};
