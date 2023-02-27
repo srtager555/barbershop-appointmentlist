3fa94df8cc35666f1b72908f40d7a689a5ceaf7d
Carlos && Ponce <srtager555@gmail.com>
2023-02-26 19:28:50 -0600

    i fixed a issue, user_id is undefiend

--- src/pages/api/appointments/userAppointment.ts
+++ src/pages/api/appointments/userAppointment.ts
@@ -1,19 +1,22 @@
 import { isExpiretAppointment } from "@common/expireAppointment";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { user_id } = JSON.parse(req.body);
+export default async function UserAppointment(req: NextApiRequest, res: NextApiResponse) {
+	const { user_id }: { user_id: user_id } = JSON.parse(req.body);
 
-	const appointment: rawAppointments | null | undefined = await Prisma?.appointments.findUnique({
-		where: {
-			user_id,
-		},
-	});
+	if (user_id) {
+		const appointment: rawAppointments | null | undefined =
+			await Prisma?.appointments.findUnique({
+				where: {
+					user_id,
+				},
+			});
 
-	const expire = isExpiretAppointment(appointment);
+		const expire = isExpiretAppointment(appointment);
 
-	res.status(200).json({
-		appointment,
-		expire,
-	});
+		res.status(200).json({
+			data: appointment,
+			expire,
+		});
+	}
 }
