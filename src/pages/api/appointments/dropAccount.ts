2c8b045a4d2357b681fb2f87f9e95e0cedf9c77a
Carlos && Ponce <srtager555@gmail.com>
2023-03-01 01:05:29 -0600

    Issue about the user appointments solved

--- src/pages/api/appointments/dropAccount.ts
+++ src/pages/api/appointments/dropAccount.ts
@@ -1,15 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@ddbb/prismadb";
 
 export default async function handlerDropAccount(req: NextApiRequest, res: NextApiResponse) {
 	let delete_appoitnment: rawAppointments | string | undefined = "sin reservas";
 	const user_id: number = JSON.parse(req.body).user_id;
 
-	const { data } = await fetch("/api/appointments/userAppointment/", {
-		method: "POST",
-		body: JSON.stringify({ user_id }),
-	}).then((data) => data.json());
+	// const { data } = await fetch("/api/appointments/userAppointment", {
+	// 	method: "POST",
+	// 	body: JSON.stringify({ user_id }),
+	// }).then((data) => data.json());
 
-	if (data) {
+	const appoint = await Prisma.appointments.findUnique({
+		where: {
+			user_id,
+		},
+	});
+
+	if (appoint) {
 		delete_appoitnment = await Prisma?.appointments.delete({
 			where: {
 				user_id,
