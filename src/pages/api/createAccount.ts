5e3aa9218d06da88018cf094281f6dfade126ffe
Carlos && Ponce <srtager555@gmail.com>
2023-03-01 01:05:14 -0600

    I did ident the code

--- src/pages/api/createAccount.ts
+++ src/pages/api/createAccount.ts
@@ -1,29 +1,29 @@
-import Prisma from "@ddbb/prismadb";
+import { Prisma } from "@ddbb/prismadb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { hash } from "bcrypt";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { name, phone, password } = req.body
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { name, phone, password } = req.body;
 
-  const exists = await Prisma.users.findUnique({
-    where: {
-      phone,
-    },
-  });
+	const exists = await Prisma.users.findUnique({
+		where: {
+			phone,
+		},
+	});
 
-  if (exists) {
-    res.status(400).send("User already exists");
-  } else {
-    const user = await Prisma.users.create({
-      data: {
-        name,
-        phone,
-        password: await hash(password, 10),
-      },
-    });
-    res.status(200).json(user);
-  }
+	if (exists) {
+		res.status(400).send("User already exists");
+	} else {
+		const hashPassword = await hash(password, 10);
+
+		const user = await Prisma.users.create({
+			data: {
+				name,
+				phone,
+				password: hashPassword,
+			},
+		});
+
+		res.status(200).json(user);
+	}
 }
