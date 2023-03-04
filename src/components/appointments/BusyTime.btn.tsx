5373c1f37d806ee5f6b97b9b8574052df5b20a79
Carlos && Ponce <srtager555@gmail.com>
2023-03-03 19:53:25 -0600

    I started to created the button to manage the appointment

--- src/components/appointments/BusyTime.btn.tsx
+++ src/components/appointments/BusyTime.btn.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { VALITD_TIME_LISTENER } from "src/utils/expiredAppointmentChecker";
+import { useSession } from "next-auth/react";
 
 import styles from "@styles/citas.module.scss";
 
 export const BusyTimeBTN = ({ time, stateStyles, callback, date }: appointmentsButtons) => {
 	const [availableTime, setAvailableTime] = useState(false);
+	const { data: session, status } = useSession();
 
 	useEffect(() => {
 		const LISTENER = VALITD_TIME_LISTENER({ time, date, availableTime, setAvailableTime });
@@ -16,7 +18,23 @@ export const BusyTimeBTN = ({ time, stateStyles, callback, date }: appointmentsB
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return (
+	return status === "authenticated" && session.user.role === "admin" ? (
+		<button>
+			<div className={styles["appointment-info"]}>
+				<span className={styles.time}>{time}</span>
+				<span className={styles.line}></span>
+				<span className={stateStyles}>reservado</span>
+			</div>
+			<div className={styles["appointment-info--extended"]}>
+				<div className={styles.imageContainer}></div>
+				<div className={styles["container--data"]}>
+					<p className={styles["user--name"]}>{session.user.name}</p>
+					<p className={styles["user--phone"]}>{session.user.phone}</p>
+					<button>Cancelar reserva</button>
+				</div>
+			</div>
+		</button>
+	) : (
 		<button
 			onClick={() => callback && callback()}
 			className={`${styles["appointment-btn"]} ${styles["reserved-appointment"]}`}
