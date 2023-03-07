ee4bcce4ea4f28e47973105ca20be169f8cd7a04
Carlos && Ponce <srtager555@gmail.com>
2023-03-07 17:56:48 -0600

    typing error in the import fixed

--- src/utils/expiredAppointmentChecker.ts
+++ src/utils/expiredAppointmentChecker.ts
@@ -5,14 +5,14 @@ interface isExpiredAppopintment {
 	setAvailableTime: any;
 }
 
-import { CURRENT_HOUR, TIME_LIST, TODAY_CUSTOM } from "@common/timeData";
+import { CURRENT_HOUR, TIME_LIST, TODAY_CUSTOM_EN } from "@common/timeData";
 
 export const VALID_TIME_TO_APPOINT = (time: time, date?: string) => {
 	const CURRENT_TIME_INDEX = TIME_LIST.indexOf(CURRENT_HOUR());
 	const BTN_TIME_INDEX = TIME_LIST.indexOf(time);
 	const threshold = 3; // Voy a dejar de trabajar en este feature porque es bastante abtracto por ahora
 
-	if (date === TODAY_CUSTOM) if (BTN_TIME_INDEX < CURRENT_TIME_INDEX) return true;
+	if (date === TODAY_CUSTOM_EN) if (BTN_TIME_INDEX < CURRENT_TIME_INDEX) return true;
 
 	return false;
 };
