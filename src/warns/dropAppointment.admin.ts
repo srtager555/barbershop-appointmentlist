9259f68f4669c7d707bb2f930f9d74c6a9764bbc
Carlos && Ponce <srtager555@gmail.com>
2023-03-06 18:30:05 -0600

    warn when an Admin going to drop an Appointment created

--- src/warns/dropAppointment.admin.ts
+++ src/warns/dropAppointment.admin.ts
@@ -1,1 +1,14 @@
 import Swal from "sweetalert2";
+
+export function AdminDropAppointmentWarn() {
+	return Swal.fire({
+		title: "¡Advertencia!",
+		icon: "warning",
+		text: "La acción de cancelar una reserva no se puede deshacer, ¿Estas seguro de cancelar la cita?",
+		showCancelButton: true,
+		cancelButtonText: "¡Mejor no!",
+		cancelButtonColor: "blue",
+		confirmButtonText: "Cancelar cita",
+		confirmButtonColor: "#f80000",
+	}).then((data) => data.isConfirmed);
+}
