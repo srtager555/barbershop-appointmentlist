67f3984920edbb8073d89396add268f267d3e070
Carlos && Ponce <srtager555@gmail.com>
2023-03-08 16:51:29 -0600

    Logic to set disable the closed time added

--- src/components/appointments/ClosedTime.btn.tsx
+++ src/components/appointments/ClosedTime.btn.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 import styles from "@styles/citas.module.scss";
 import { handlerCloseAndOpenAnAppointment } from "@utils/closeAndOpenAppoinment";
+import { VALITD_TIME_LISTENER } from "@utils/expiredAppointmentChecker";
 
 export const ClosedTimeBTN = ({ time, date, stateStyles }: appointmentsButtons) => {
+	const [availableTime, setAvailableTime] = useState(false);
 	const { data: session, status } = useSession();
 	const role = status === "authenticated" && session.user.role;
+
+	useEffect(() => {
+		const LISTENER = VALITD_TIME_LISTENER({ time, date, availableTime, setAvailableTime });
+
+		return () => {
+			clearInterval(LISTENER);
+		};
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<button
@@ -14,6 +27,7 @@ export const ClosedTimeBTN = ({ time, date, stateStyles }: appointmentsButtons) 
 				if (status === "authenticated")
 					if (session.user.role === "admin") handlerCloseAndOpenAnAppointment(date, time);
 			}}
+			disabled={availableTime}
 		>
 			<span className={styles.time}>{time}</span>
 			<span className={styles.line}></span>
