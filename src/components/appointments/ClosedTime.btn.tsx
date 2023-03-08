2826743e5caa5004999cc539ecbec2273ce22740
Carlos && Ponce <srtager555@gmail.com>
2023-03-08 03:37:26 -0600

    logic to open the appointment created

--- src/components/appointments/ClosedTime.btn.tsx
+++ src/components/appointments/ClosedTime.btn.tsx
@@ -1,9 +1,25 @@
-import styles from "@styles/citas.module.scss";
+import { useSession } from "next-auth/react";
 
-export const ClosedTimeBTN = ({ time, stateStyles }: appointmentsButtons) => (
-	<button className={`${styles["appointment-btn"]} ${styles.close}`}>
-		<span className={styles.time}>{time}</span>
-		<span className={styles.line}></span>
-		<span className={stateStyles}>PUERTAS CERRADAS</span>
-	</button>
-);
+import styles from "@styles/citas.module.scss";
+import { handlerCloseAndOpenAnAppointment } from "@utils/closeAndOpenAppoinment";
+
+export const ClosedTimeBTN = ({ time, date, stateStyles }: appointmentsButtons) => {
+	const { data: session, status } = useSession();
+	const role = status === "authenticated" && session.user.role;
+
+	return (
+		<button
+			className={`${styles["appointment-btn"]} ${styles.close}`}
+			onClick={() => {
+				if (status === "authenticated")
+					if (session.user.role === "admin") handlerCloseAndOpenAnAppointment(date, time);
+			}}
+		>
+			<span className={styles.time}>{time}</span>
+			<span className={styles.line}></span>
+			<span className={stateStyles}>
+				{role === "admin" ? "Abrir puertas" : "PUERTAS CERRADAS"}
+			</span>
+		</button>
+	);
+};
