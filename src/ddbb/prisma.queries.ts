03eac412e575997e9aedeb8408fb077c6f3cb22f
Carlos && Ponce <srtager555@gmail.com>
2023-03-09 17:27:44 -0600

    I removed the ? from the Prisma client

--- src/ddbb/prisma.queries.ts
+++ src/ddbb/prisma.queries.ts
@@ -5,18 +5,18 @@ export const LAYOUT_DAY = async (date: string) => {
 	const DAY_NUMBER = new Date(date).getDay();
 
 	const BASE =
-		(await Prisma?.layout_day.findUnique({
+		(await Prisma.layout_day.findUnique({
 			where: {
 				day: week[DAY_NUMBER],
 			},
 		})) ||
-		(await Prisma?.layout_day.findUnique({
+		(await Prisma.layout_day.findUnique({
 			where: {
 				day: "default",
 			},
 		}));
 
-	const CUSTOM_BASE = await Prisma?.custom_oc.findUnique({
+	const CUSTOM_BASE = await Prisma.custom_oc.findUnique({
 		where: {
 			date,
 		},
@@ -26,14 +26,14 @@ export const LAYOUT_DAY = async (date: string) => {
 };
 
 export const DATE_APOINTMENTS = async (date: string) =>
-	await Prisma?.appointments.findMany({
+	await Prisma.appointments.findMany({
 		where: {
 			date: date,
 		},
 	});
 
 export const DATE_CLOSED_TIME = async (date: string) =>
-	await Prisma?.custom_closed_time.findMany({
+	await Prisma.custom_closed_time.findMany({
 		where: {
 			date: date,
 		},
