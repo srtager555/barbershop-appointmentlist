c830d75828b809b76216b89e80a28acf0bd65831
Carlos && Ponce <srtager555@gmail.com>
2023-03-10 01:13:47 -0600

    I created a util file to upload the user image

--- src/utils/uploadUserImage.ts
+++ src/utils/uploadUserImage.ts
@@ -1,18 +1,35 @@
 import { supabase } from "@ddbb/supabase.client";
 import { Session } from "next-auth/core/types";
 import { RefObject } from "react";
+import { DeleteUserImage } from "./deleteUserImage";
 
 export const handlerUploadUserAvatar = async (
 	imageInputRef: RefObject<HTMLInputElement>,
-	session: Session
+	session: Session | null
 ) => {
 	if (!imageInputRef.current) return;
 
 	const image = imageInputRef.current.files;
 
-	if (!image) return;
+	if (!image || !session) return;
 
-	const { data, error } = await supabase.storage
-		.from("user-image")
-		.upload(`avatar_${session.user.id}`, image[0]);
+	try {
+		const { data, error } = await supabase.storage
+			.from("user-image")
+			.upload(`avatar_${session.user.id}.${image[0].type.split("/")[1]}`, image[0], {
+				upsert: true,
+			});
+
+		if (error) throw error;
+
+		await fetch("/api/user/setImagePath", {
+			method: "POST",
+			body: JSON.stringify({
+				path: data.path,
+				user_id: session.user.id,
+			}),
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
