420a69137551edda91ecddd0b2f320c066bbf448
Carlos && Ponce <srtager555@gmail.com>
2023-03-13 04:51:28 -0600

    Im testing the functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,42 @@ import Link from "next/link";
 import type { NextPage } from "next";
 
 import styles from "@styles/Home.module.scss";
+import { supabase } from "@ddbb/supabase.client";
+import { useEffect } from "react";
 
-const Home: NextPage = () => (
-	<div className={styles.container}>
-		<div className={styles["container-content"]}>
-			<h1 className={styles.title}>Barberia Jossiel</h1>
-			<Link href="/citas" className={styles["circle-link"]}>
-				<span>Entrar</span>
-			</Link>
+const Home: NextPage = () => {
+	const handlerTesting = async () => {
+		const { data, error } = await supabase.functions.invoke("noti", {
+			body: {
+				name: "Carlos",
+			},
+		});
+
+		return data;
+	};
+
+	useEffect(() => {
+		console.log(handlerTesting());
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles["container-content"]}>
+				<h1 className={styles.title}>Barberia Jossiel</h1>
+				<Link href="/citas" className={styles["circle-link"]}>
+					<span>Entrar</span>
+				</Link>
+			</div>
+			<div className={styles["social-networks"]}>
+				<a href="" target="_blank">
+					instagram
+				</a>
+				<a href="" target="_blank">
+					facebook
+				</a>
+			</div>
 		</div>
-		<div className={styles["social-networks"]}>
-			<a href="" target="_blank">
-				instagram
-			</a>
-			<a href="" target="_blank">
-				facebook
-			</a>
-		</div>
-	</div>
-);
+	);
+};
 
 export default Home;
